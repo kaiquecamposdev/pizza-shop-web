9f99ffc98ae764c516279d372b88fef37e50a351
Kaique <kaiquedecampos2@gmail.com>
2024-05-02 19:13:48 -0300

    feat: create sign in

---
+++ src/pages/auth/sing-in.tsx
@@ -0,0 +1,36 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export function SignIn() {
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-full max-w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-center text-2xl font-bold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-center text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel de atendimento!
+            </p>
+          </div>
+
+          <form action="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu email</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button type="submit" className="w-full bg-primary">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
