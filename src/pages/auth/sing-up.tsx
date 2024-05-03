1383ef6bdf492a19b3d4205dfbd28eb1a6f39c84
Kaique <kaiquedecampos2@gmail.com>
2024-05-03 19:05:09 -0300

    feat: create sign-up

---
+++ src/pages/auth/sing-up.tsx
@@ -0,0 +1,100 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const SignUpFormSchema = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  email: z.string().email(),
+  phone: z.string(),
+})
+
+type SignUpForm = z.infer<typeof SignUpFormSchema>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    console.log(data.email)
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    toast.success('Restaurante cadastrado com sucesso.', {
+      action: {
+        label: 'Login',
+        onClick: () => navigate('/sign-in'),
+      },
+    })
+  }
+
+  return (
+    <>
+      <Helmet title="Cadastro" />
+      <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Fazer Login</Link>
+        </Button>
+        <div className="flex w-full max-w-[350px] flex-col justify-center gap-4">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-center text-2xl font-bold tracking-tight">
+              Criar conta
+            </h1>
+            <p className="text-center text-sm text-muted-foreground">
+              Seja um parceiro e comece suas vendas!
+            </p>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+            <div className="space-y-2">
+              <Label htmlFor="name">Nome do estabelecimento</Label>
+              <Input id="name" type="text" {...register('restaurantName')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="name">Seu nome</Label>
+              <Input id="name" type="text" {...register('managerName')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="text" {...register('phone')} />
+            </div>
+
+            <Button
+              disabled={isSubmitting}
+              type="submit"
+              className="w-full bg-primary"
+            >
+              Finalizar Cadastro
+            </Button>
+
+            <p className="text-center text-sm text-muted-foreground">
+              Ao continuar, você concorda com nossos{' '}
+              <a href="" className="underline underline-offset-4">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a href="" className="underline underline-offset-4">
+                políticas de privacidade.
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sing-in'
+import { SignUp } from './pages/auth/sing-up'
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +24,16 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+    ],
+  },
+  {
+    path: '/sign-up',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
