53b12288185018c855ba3d0361ad0b2d746a151b
Kaique <kaiquedecampos2@gmail.com>
2024-05-11 16:50:07 -0300

    feat: create theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
   return (
-    <div className="border-b">
+    <div className="flex items-center border-b">
       <header className="flex h-16 items-center gap-6 px-6">
         <Pizza size={24} />
         <Separator orientation="vertical" className="h-6" />
@@ -20,6 +21,9 @@ export function Header() {
           </NavLink>
         </nav>
       </header>
+      <div className="ml-auto mr-4">
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
